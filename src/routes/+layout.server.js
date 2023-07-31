d32e8a9b000535ca9c9309b95291de9ae09b80d4
laurielim <laurie.limsam@gmail.com>
2023-07-31 10:14:45 +0300

    Move import to layour.server.js directly

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,8 +1,23 @@
-import { getAllLessons } from '$lib/data/lessons';
-
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
+	const data = import.meta.glob('/static/lessons/*.json')
+	/**
+	 * @type {{ slug: string; id: number; title: string; description: string; }[]}
+	 */
+	const lessons = [];
+	for (const path in data) {
+		data[path]().then((mod) => {
+			const slug = path.replace('/static', '').replace(/\.json$/, '');
+
+			lessons.push({
+				slug,
+				id: mod.id,
+				title: mod.title,
+				description: mod.description,
+			})
+		})
+	}
 	return {
-		lessons: getAllLessons()
+		lessons: lessons.sort((a, b) => a.id - b.id)
 	};
 }
